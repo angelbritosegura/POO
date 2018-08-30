4b632177508627f9547708b4a5669e687aeab09d
antovanFI <31055591+antovanFI@users.noreply.github.com>
2018-08-29 23:59:02 -0500

    Clase previa a Práctica 3

---
+++ Argumentos.java
@@ -0,0 +1,9 @@
+public class Argumentos {
+	public static void main(String[] args){
+		String nombre, lista, calificacion;
+		nombre = args[0];
+		lista = args[1];
+		calificacion = args[2];
+		System.out.println("Hola "+nombre+" tu número de lista es "+lista+" y sacaste "+calificacion);
+	}
+}
