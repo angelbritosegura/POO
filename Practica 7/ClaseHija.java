a036f54c34961e392bb1caee3bff8d9f83168143
antovanFI <31055591+antovanFI@users.noreply.github.com>
2018-09-27 11:54:25 -0500

    con error en metodo final

--- Practica 7/ClaseHija.java
+++ Practica 7/ClaseHija.java
@@ -1,5 +1,12 @@
 package practica7;
 
 public class ClaseHija extends ClasePadre{
+	public void metodoA(){
+		 System.out.println("Es la version de la clase hija que se ejecuta");
+		 super.metodoA();
+	}
 
+	public void metodoFinal(){
+		System.out.println("Se ejecuta metodoFinal() desde la clase hija");
+	}
 }
